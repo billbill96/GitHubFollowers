f13a5833c52ab04464282dfcd467fc4ce9fa0d77
Supannee Mutitanon <supannee.muu@gmail.com>
2021-06-23 22:39:04 +0700

    data source

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class FollowerListViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
 
     var username: String!
+    var followers: [Followers] = []
+    
     var collectionView: UICollectionView!
+    var datasource: UICollectionViewDiffableDataSource<Section, Followers>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,7 @@ class FollowerListViewController: UIViewController {
         configureCollectionView()
         configureViewController()
         getFollowers()
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,7 +34,6 @@ class FollowerListViewController: UIViewController {
     }
     
     func configureViewController() {
-        view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
@@ -35,7 +42,8 @@ class FollowerListViewController: UIViewController {
             switch result {
             case .success(let followers):
                 print("followers count \(followers.count)")
-                print(followers)
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happed.",
                                                 message: error.rawValue,
@@ -43,17 +51,43 @@ class FollowerListViewController: UIViewController {
             }
         }
     }
-    
-    func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
-        view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemPink
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCollectionViewCell.self,
                                 forCellWithReuseIdentifier: FollowerCollectionViewCell.reuseID)
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
+        collectionView.alwaysBounceVertical = true
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaiableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaiableWidth / 3 //3 is number in row
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        return flowLayout
+    }
+
+    func configureDataSource() {
+        datasource = UICollectionViewDiffableDataSource<Section, Followers>(collectionView: collectionView,
+                                                                            cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCollectionViewCell.reuseID,
+                                                          for: indexPath) as! FollowerCollectionViewCell
+            cell.set(followers: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Followers>()
+        snapShot.appendSections([.main]) //add section
+        snapShot.appendItems(followers)
+        datasource.apply(snapShot, animatingDifferences: true)
     }
 }
-
-//extension FollowerListViewController
