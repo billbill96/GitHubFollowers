a274df39baae0a29557a6737c23cf17bcc088c2a
Supannee Mutitanon <supannee.muu@gmail.com>
2021-10-09 16:24:09 +0700

    add favoirte

--- GitHubFollowers/Screens/FavoriteListViewController.swift
+++ GitHubFollowers/Screens/FavoriteListViewController.swift
@@ -12,6 +12,13 @@ class FavoriteListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
